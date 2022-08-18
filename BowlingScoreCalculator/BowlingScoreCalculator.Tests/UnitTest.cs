7d0903b6d73570d2bfb7bc87bcdba95868c476af
Nikola Knezevic <n.knezevic@victory.rs>
2022-08-19 01:53:59 +0200

    initialize the solution

---
+++ BowlingScoreCalculator/BowlingScoreCalculator.Tests/UnitTest.cs
@@ -0,0 +1,15 @@
+namespace BowlingScoreCalculator.Tests;
+
+public class CalculatorTests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
