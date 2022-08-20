962ed87538b0c480b48a4766b529e44fe059b7dc
Nikola Knezevic <n.knezevic@victory.rs>
2022-08-20 03:23:20 +0200

    add perfect game scenario

---
+++ BowlingScoreCalculator/BowlingScoreCalculator.Api/Bowling/Calculator.cs
@@ -0,0 +1,35 @@
+﻿namespace BowlingScoreCalculator.Api.Bowling;
+
+public class Calculator
+{
+    private readonly ILogger<Calculator> logger;
+
+    public Calculator(ILogger<Calculator> logger)
+    {
+        this.logger = logger;
+        logger.LogDebug($"Created instance of {nameof(Calculator)}");
+    }
+
+    public GameResult Calculate(IEnumerable<int> rolls)
+    {
+        var rollsArr = rolls.ToArray();
+        var runningTotal = 0;
+        var scores = new List<int>();
+        var finished = true;
+        for (int i = 0; i < rollsArr.Length; i += 2)
+        {
+            if (rollsArr[i] == 10) //strike
+            {
+                runningTotal += rollsArr[i] + rollsArr[i + 1] + rollsArr[i + 2];
+                scores.Add(runningTotal);
+                if (i + 2 >= rollsArr.Length - 1)
+                {
+                    break;
+                }
+                i--;
+                continue;
+            }
+        }
+        return new GameResult(scores) { IsCompleted = finished };
+    }
+}
---
+++ BowlingScoreCalculator/BowlingScoreCalculator.Api/Bowling/GameResult.cs
@@ -0,0 +1,12 @@
+﻿namespace BowlingScoreCalculator.Api.Bowling;
+
+public class GameResult
+{
+    public bool IsCompleted { get; set; }
+    public IEnumerable<int> Scores { get; set; }
+
+    public GameResult(IEnumerable<int> scores)
+    {
+        Scores = scores;
+    }
+}
