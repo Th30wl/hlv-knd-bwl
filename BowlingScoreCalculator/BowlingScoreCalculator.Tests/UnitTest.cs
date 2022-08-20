09e996aa6b2358e7955afa3e131fedac606a2886
Nikola Knezevic <n.knezevic@victory.rs>
2022-08-20 03:23:20 +0200

    add perfect game scenario

--- BowlingScoreCalculator/BowlingScoreCalculator.Tests/UnitTest.cs
+++ BowlingScoreCalculator/BowlingScoreCalculator.Tests/UnitTest.cs
@@ -1,15 +1,20 @@
+using BowlingScoreCalculator.Api.Bowling;
+using Microsoft.Extensions.Logging.Abstractions;
+
 namespace BowlingScoreCalculator.Tests;
 
 public class CalculatorTests
 {
-    [SetUp]
-    public void Setup()
-    {
-    }
-
     [Test]
-    public void Test1()
+    public void AllStrikesFinishedGame()
     {
-        Assert.Pass();
+        var calc = new Calculator(new NullLogger<Calculator>());
+        var rolls = new int[] { 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10 };
+        var result = calc.Calculate(rolls);
+        Assert.Multiple(() => {
+            Assert.That(result.IsCompleted, Is.True);
+            Assert.That(result.Scores.Last(), Is.EqualTo(300));
+            Assert.That(result.Scores.Count(), Is.EqualTo(10));
+        });
     }
 }
