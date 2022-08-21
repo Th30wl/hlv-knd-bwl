33ec7f5a9804f49b0fb7846e71ff4c9a331e2b82
Nikola Knezevic <n.knezevic@victory.rs>
2022-08-21 02:44:36 +0200

    add unfinished game calculation

--- BowlingScoreCalculator/BowlingScoreCalculator.Api/Bowling/Calculator.cs
+++ BowlingScoreCalculator/BowlingScoreCalculator.Api/Bowling/Calculator.cs
@@ -14,30 +14,49 @@ public class Calculator
     {
         var rollsArr = rolls.ToArray();
         var runningTotal = 0;
-        var scores = new List<int>();
+        var scores = new List<int?>();
         var finished = true;
         for (int i = 0; i < rollsArr.Length; i += 2)
         {
+            if (i + 1 >= rollsArr.Length)
+            {
+                if (scores.Count < 10)
+                {
+                    scores.Add(null);
+                }
+                break;
+            }
             var score = rollsArr[i] + rollsArr[i + 1];
+
             if (score < 10) //no bonus pts
             {
                 runningTotal += score;
                 scores.Add(runningTotal);
                 continue;
             }
+            
+            if (i + 2 >= rollsArr.Length)
+            {
+                if (scores.Count < 10)
+                {
+                    scores.Add(null);
+                }
+                break;
+            }
 
             score += rollsArr[i + 2]; //spare
+
             runningTotal += score;
             scores.Add(runningTotal);
 
-            if (i + 2 >= rollsArr.Length - 1)
-            {
-                break;
-            }
             if (rollsArr[i] == 10) //strike, frame ends with first roll
             {
                 i--;
             }
+        }
+        if (scores.Count < 10 || scores.Last() == null)
+        {
+            finished = false;
         }
         return new GameResult(scores) { IsCompleted = finished };
     }
