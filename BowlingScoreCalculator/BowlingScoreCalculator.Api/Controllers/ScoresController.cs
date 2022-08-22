d762048043aeb03d3fbcabc1b687d4f9ab58653d
Nikola <code.nikola@gmail.com>
2022-08-23 01:54:31 +0200

    implement calculator controller

--- BowlingScoreCalculator/BowlingScoreCalculator.Api/Controllers/ScoresController.cs
+++ BowlingScoreCalculator/BowlingScoreCalculator.Api/Controllers/ScoresController.cs
@@ -1,22 +1,47 @@
+using BowlingScoreCalculator.Api.Bowling;
 using BowlingScoreCalculator.Api.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BowlingScoreCalculator.Api.Controllers;
 
+/// <summary>
+/// Bowling score calculator
+/// </summary>
 [ApiController]
 [Route("/scores")]
 public class ScoresController : ControllerBase
 {
     private readonly ILogger<ScoresController> logger;
+    private readonly ICalculator calculator;
 
-    public ScoresController(ILogger<ScoresController> logger)
+    public ScoresController(ILogger<ScoresController> logger, ICalculator calculator)
     {
+        logger.LogDebug($"Created instance of {nameof(ScoresController)}");
         this.logger = logger;
+        this.calculator = calculator;
     }
 
+    /// <summary>
+    /// Calculates the score of a bowling game
+    /// </summary>
+    /// <param name="request">Information about the pins downed</param>
+    /// <returns>Calculated scores and info if the game is completed and valid</returns>
     [HttpPost]
     public CalculationResponse Post(CalculationRequest request)
     {
-        throw new NotImplementedException();
+        try
+        {
+            var result = calculator.Calculate(request.PinsDowned);
+            return new CalculationResponse
+            {
+                FrameProgressScores = result.Scores.Select(x => x?.ToString() ?? "*"),
+                GameCompleted = result.IsCompleted
+            };
+        }
+        catch (Exception e)
+        {
+            logger.LogError("An error occured while calculating game score", e);
+            return new CalculationResponse { GameValid = false };
+        }
     }
 }
--- BowlingScoreCalculator/BowlingScoreCalculator.Api/Program.cs
+++ BowlingScoreCalculator/BowlingScoreCalculator.Api/Program.cs
@@ -1,3 +1,7 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.PlatformAbstractions;
+using System.Reflection;
+
 namespace BowlingScoreCalculator.Api;
 
 public class Program
@@ -5,6 +9,8 @@ public class Program
     public static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+        var workingDir = PlatformServices.Default.Application.ApplicationBasePath;
+        var assemblyName = typeof(Program).GetTypeInfo().Assembly.GetName().Name;
 
         // Add services to the container.
 
@@ -12,6 +18,11 @@ public class Program
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
         builder.Services.AddSwaggerGen();
+        builder.Services.AddSingleton<Bowling.ICalculator, Bowling.Calculator>();
+        builder.Services.AddSwaggerGen((o) =>
+        {
+            o.IncludeXmlComments(Path.Combine(workingDir, assemblyName + ".xml"));
+        });
 
         var app = builder.Build();
 
@@ -21,9 +32,6 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
-
-        app.UseAuthorization();
-
 
         app.MapControllers();
 
